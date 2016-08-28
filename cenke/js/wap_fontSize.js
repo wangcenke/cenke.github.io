ea1ae681a92d60fe396e1f6a629ddebe88e52a5a
wanghuiru <173957007@qq.com>
2016-08-28 14:02:39 +0800

    1

---
+++ cenke/js/wap_fontSize.js
@@ -0,0 +1,16 @@
+// js适配rem根字体大小
+(function (doc, win) {
+        var docEl = doc.documentElement,
+            resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+            recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = 20 * (clientWidth / 320) + 'px';
+        };
+        if (!doc.addEventListener) return;
+        win.addEventListener(resizeEvt, recalc, false);
+        doc.addEventListener('DOMContentLoaded', recalc, false);
+    })(document, window);
+
+ //clientWidth=750px(23*),clientWidth=640px(20*),clientWidth=480px(15*),
+ // clientWidth=384px(12*),clientWidth=320px(10*)
